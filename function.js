d6298bb3a34035b2f3b736f0838aea45a6b84ee9
sara-ba <127237204+sara-ba@users.noreply.github.com>
2023-03-08 17:27:10 -0300

    Delete function.js

--- function.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('boton').onclick= function () {
-    
-    console.log ("capturamos el evento clik");
-    document.getElementById ("demo").innerHTML= "Celular:2236888888  Mail: rosaperez@gmail.com"
-}
-
